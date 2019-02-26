ba3cb793396293d19a84b55cdbd571da9c07c3f1
snt_danilo_markovic <dmarkovic@smartnet.rs>
2019-02-26 13:08:08 +0100

    added error handling.

--- activities/mytask-status.js
+++ activities/mytask-status.js
@@ -1,7 +1,6 @@
 'use strict';
 
-const logger = require('@adenin/cf-logger');
-const handleError = require('@adenin/cf-activity').handleError;
+const cfActivity = require('@adenin/cf-activity');
 const api = require('./common/api');
 
 module.exports = async (activity) => {
@@ -9,6 +8,10 @@ module.exports = async (activity) => {
     api.initialize(activity);
     //const response = await api('/tasks?filter=overdue|today');
     const response = await api('/tasks');
+
+    if (!cfActivity.isResponseOk(activity, response)) {
+      return;
+    }
 
     let taskStatus = {
       title: 'Active Tasks',
@@ -37,6 +40,7 @@ module.exports = async (activity) => {
     activity.Response.Data = taskStatus;
 
   } catch (error) {
-    handleError(error, activity);
+    
+    cfActivity.handleError(error, activity);
   }
 };
--- activities/tasks.js
+++ activities/tasks.js
@@ -1,26 +1,24 @@
-const api = require('./common/api');
+'use strict';
 
+const cfActivity = require('@adenin/cf-activity');
+const api = require('./common/api');
 
 module.exports = async function (activity) {
 
   try {
-
     api.initialize(activity);
     //const response = await api('/tasks?filter=overdue|today');
     const response = await api('/tasks');
+
+    if (!cfActivity.isResponseOk(activity, response)) {
+      return;
+    }
 
     // convert response to items[]
     activity.Response.Data = api.convertIssues(response);
 
   } catch (error) {
 
-    // return error response
-    var m = error.message;
-    if (error.stack) m = m + ": " + error.stack;
-
-    activity.Response.ErrorCode = (error.response && error.response.statusCode) || 500;
-    activity.Response.Data = { ErrorText: m };
-
+    cfActivity.handleError(error, activity);
   }
-
 };
